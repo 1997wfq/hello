f8bc998578ea49c65f488867677d9a7fae3acdcc
wufangqing <1091712098@qq.com>
2021-09-13 01:50:49 +0800

    first 添加 Hello.java文件

---
+++ hello/src/com/haik/Hello.java
@@ -0,0 +1,11 @@
+package com.haik;
+//wowudi
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("sss");
+        float a = 0.1f;
+        double b = 1.0/10;
+        System.out.println(a==b);
+    }
+}
